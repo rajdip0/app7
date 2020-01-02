ce0dbe2ecfc6333d15f23e948fbaf170e23626ca
rajdip34 <rajdipmondal34@gmail.com>
2020-01-02 15:58:39 +0530

    first update

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:app7/screens/home.dart';
+import 'package:flutter/material.dart';
+
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Material App',
+      home: Scaffold(
+        body: HomePage(),
+        backgroundColor: Colors.black,
+      ),
+    );
+  }
+}
+ 
